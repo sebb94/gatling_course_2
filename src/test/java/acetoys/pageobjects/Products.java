77c21e54a92ef17459537653056f4e7f479220e1
sebb94 <c>
2022-12-27 11:54:56 +0100

    product feeder json

--- src/test/java/acetoys/pageobjects/Products.java
+++ src/test/java/acetoys/pageobjects/Products.java
@@ -1,37 +1,28 @@
 package acetoys.pageobjects;
 
 import io.gatling.javaapi.core.ChainBuilder;
+import io.gatling.javaapi.core.FeederBuilder;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
 
 public class Products {
 
-  public static ChainBuilder loadProductDetailsPage_DartBoards = 
+  public static final FeederBuilder<Object> productFeeder = jsonFile("data/productDetails.json");
+
+  public static ChainBuilder loadProductDetailsPage = 
+  feed(productFeeder).
   exec(
-    http("Load products details page - Product: Darts Board")
-      .get("/product/darts-board")
-      .check(css("#ProductDescription").is("Get all your mates round for a few drinks and throw sharp objects at this darts board"))
+    http("Load products details page - Product: #{name}")
+      .get("/product/#{slug}")
+      .check(css("#ProductDescription").isEL("#{description}"))
   );
 
-  public static ChainBuilder addProductToCart_Product19 = 
+  public static ChainBuilder addProductToCart = 
   exec(
-    http("Add product to cart - product id 19")
-      .get("/cart/add/19")
+    http("Add product to cart - Product name #{name}")
+      .get("/cart/add/#{id}")
       .check(substring("You have <span>1</span> products in your Basket"))
   );
 
-  public static ChainBuilder addProductToCart_Product4 = 
-  exec(
-    http("Add product to cart - product id 4")
-      .get("/cart/add/4")
-      .check(substring("You have <span>2</span> products in your Basket"))
-  );
-
-  public static ChainBuilder addProductToCart_Product5 = 
-  exec(
-    http("Add product to cart - product id 5")
-      .get("/cart/add/5")
-      .check(substring("You have <span>3</span> products in your Basket"))
-  );
 }
